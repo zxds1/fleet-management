2f7f449c6cdba4dd2510e18ea91170a06d8444d2
Brian <mwirigib332@gmail.com>
2026-08-11 01:12:35 -0700

    Revert "chore: checkout mobile app source from mobile-app branch"
    This reverts commit 03b057366590f7045c88fe2db491e8952f65ef30.

--- packages/mobile/index.js
+++
@@ -1,6 +0,0 @@
-// packages/mobile/index.js
-// Expo entry point. Registers the root component with the Expo runtime.
-import { registerRootComponent } from 'expo';
-import App from './src/App';
-
-registerRootComponent(App);
